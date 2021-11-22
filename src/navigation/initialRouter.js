46e8f1ad00c222ab51674105f9b4581b37fbbc8b
mypoliticsweb <73365506+QuaeDev@users.noreply.github.com>
2021-11-22 18:00:08 -0500

    added about me page

--- src/navigation/initialRouter.js
+++ src/navigation/initialRouter.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "@/pages/landingPage.js";
+import AboutMe from "@/pages/aboutMe";
 
 export default function InitialRouter() {
 	return (
 		<div id="app-container">
 			<Routes>
+				<Route path="/about_me" element={<AboutMe />} />
 				<Route path="/" element={<LandingPage />} />
 			</Routes>
 		</div>
--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import headShot from "@/assets/picture.jpg";
 import EmailLink from "@/components/shared/emailLink";
 export default function LandingPage() {
 	return (
 		<header className="App-header">
 			<img src={headShot} className="picture" />
+			<Link to={"/about_me"} id="about_me_link">
+				<h4>About Me</h4>
+			</Link>
 			<h2>Hi, Website Coming Soon</h2>
 			<h5>
 				If you need to contact me, please email{" "}
